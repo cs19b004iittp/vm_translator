067cfb480edba98eced44c13990cd73def0752b2
CS19B021 <70951042+cs19b021iittp@users.noreply.github.com>
2022-11-24 22:23:25 +0530

    Delete tests directory

--- tests/test_parser.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from src.parser import add_two
-
-class TestVMTranslator(unittest.TestCase):
-    def test(self):
-        self.assertEqual(add_two(3), 5)
--- tests/test_vm_translator.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from VMTranslator import add_one
-
-class TestVMTranslator(unittest.TestCase):
-    def test(self):
-        self.assertEqual(add_one(3), 4)
